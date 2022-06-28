7e23a841c8dfbe37061e1a358ea5fb2abf4d45b8
erikacls <erikalopes.w3@gmail.com>
2022-06-28 13:36:22 -0300

    jsmod

---
+++ sintaxe-mod/6-objects.js
@@ -0,0 +1,11 @@
+// Objects Literals
+
+const palavra = 'id';
+
+const obj = {
+  p1: 10,
+  p2: 20,
+  f1() {},
+  f2: () => {},
+  [palavra]: 10,
+};
---
+++ sintaxe-mod/7-destructuring.js
@@ -0,0 +1,12 @@
+// Desconstruindo um Pacote
+
+// Pacote Math
+const PI = Math.PI;
+const E = Math.E;
+const SQRT2 = Math.SQRT2;
+
+const { PI, E, SQRT2 }  = Math;
+
+// Com require
+const { readFile } = require('fs');
+
